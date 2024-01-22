c1524f16a854ff9997fcc8ae66ef32213171b219
Garvit <r.garvit7@gmail.com>
2024-01-23 00:55:16 +0530

    skills section - final

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,14 +2,12 @@
 
 import Image from "next/image";
 import displayPicture from "@/public/my-passport-photo.jpg";
-import React, { useEffect } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
-import { useInView } from "react-intersection-observer";
-import { useActiveSectionContext } from "@/context/active";
 import { useSectionInView } from "@/lib/hooks";
 
 const Intro = () => {
