0e1fef52276dcfcbdfa712ef619220905e6c650d
Garvit <r.garvit7@gmail.com>
2024-01-23 00:36:58 +0530

    updating header, motion, scroll, context

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,18 +2,24 @@
 
 import Image from "next/image";
 import displayPicture from "@/public/my-passport-photo.jpg";
-import React from "react";
+import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
+import { useInView } from "react-intersection-observer";
+import { useActiveSectionContext } from "@/context/active";
+import { useSectionInView } from "@/lib/hooks";
 
 const Intro = () => {
+  const { ref } = useSectionInView("Home", 0.5);
+
   return (
     <section
       id="home"
       className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+      ref={ref}
     >
       <div className="flex items-center justify-center">
         <div className="relative">
